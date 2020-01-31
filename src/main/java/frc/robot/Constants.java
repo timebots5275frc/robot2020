38c9db710589f5c68475aa35c31dcc2d868d204d
Lucas Ferguson <LucasFerguson16@gmail.com>
2020-01-30 19:09:10 -0600

    Added Constants.Java File

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class DriveConstants {
+        public static final int kLeftMotor1CAN = 0;
+        public static final int kLeftMotor2CAN = 1;
+        public static final int kRightMotor1CAN = 2;
+        public static final int kRightMotor2CAN = 3;
+
+        public static final boolean kLeftEncoderReversed = false;
+        public static final boolean kRightEncoderReversed = true;
+
+        public static final int kEncoderCPR = 1024;
+        public static final double kWheelDiameterInches = 6;
+        public static final double kEncoderDistancePerPulse =
+                // Assumes the encoders are directly mounted on the wheel shafts
+                (kWheelDiameterInches * Math.PI) / (double) kEncoderCPR;
+    }
+}
