1ffb28250889f790570aaa90c396ef521cb3554e
Lucas <lucasferguson16@gmail.com>
2020-02-17 22:30:13 -0600

    //

--- src/main/java/frc/robot/commands/HopperSet.java
+++ src/main/java/frc/robot/commands/HopperSet.java
@@ -11,47 +11,45 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.Hopper;
 
 public class HopperSet extends CommandBase {
-	final Hopper hopperSubsystem;
+    final Hopper hopperSubsystem;
 
-	private WPI_VictorSPX hopperVictorTop;
-	private WPI_VictorSPX hopperVictorBottom;
+    private WPI_VictorSPX hopperVictorTop;
+    private WPI_VictorSPX hopperVictorBottom;
 
-	private double speedPercent;
+    private double speedPercent;
 
-	/**
-	 * @param hopperSubsystem Between -1 to 1
-	 * @param speedPercent
-	 */
-	public HopperSet(Hopper hopperSubsystem, double speedPercent) {
-		this.hopperSubsystem = hopperSubsystem;
-		this.speedPercent = speedPercent;
+    /**
+     * @param hopperSubsystem Between -1 to 1
+     * @param speedPercent
+     */
+    public HopperSet(Hopper hopperSubsystem, double speedPercent) {
+        this.hopperSubsystem = hopperSubsystem;
+        this.speedPercent = speedPercent;
 
-		addRequirements(hopperSubsystem);
-		hopperVictorTop = hopperSubsystem.getHopperVictorBottom();
-		hopperVictorBottom = hopperSubsystem.getHopperVictorTop();
-	}
+        addRequirements(hopperSubsystem);
+        hopperVictorTop = hopperSubsystem.getHopperVictorBottom();
+        hopperVictorBottom = hopperSubsystem.getHopperVictorTop();
+    }
 
-	// Called just before this Command runs the first time
-	@Override
-	public void initialize() {
+    // Called just before this Command runs the first time
+    @Override
+    public void initialize() {
 
-	}
+    }
 
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	public void execute() {
-		hopperVictorTop.set(ControlMode.PercentOutput, speedPercent);
-		hopperVictorBottom.set(ControlMode.PercentOutput, speedPercent);
-	}
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+        hopperVictorTop.set(ControlMode.PercentOutput, speedPercent);
+        hopperVictorBottom.set(ControlMode.PercentOutput, speedPercent);
+    }
 
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -8,61 +8,60 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class JoystickDrive extends CommandBase {
-	private final DriveSubsystem m_drive;
-	private final Joystick joystick;
+    private final DriveSubsystem m_drive;
+    private final Joystick joystick;
 
-	/**
-	 * Creates a new DefaultDrive.
-	 *
-	 * @param subsystem The drive subsystem this command wil run on.
-	 * @param forward   The control input for driving forwards/backwards
-	 * @param rotation  The control input for turning
-	 */
-	public JoystickDrive(DriveSubsystem subsystem, Joystick joystick) {
-		this.m_drive = subsystem;
-		this.joystick = joystick;
-		addRequirements(subsystem);
-	}
+    /**
+     * Creates a new DefaultDrive.
+     *
+     * @param subsystem The drive subsystem this command wil run on.
+     * @param forward   The control input for driving forwards/backwards
+     * @param rotation  The control input for turning
+     */
+    public JoystickDrive(DriveSubsystem subsystem, Joystick joystick) {
+        this.m_drive = subsystem;
+        this.joystick = joystick;
+        addRequirements(subsystem);
+    }
 
-	// Called when the command is initially scheduled.
-	@Override
-	public void initialize() {
-	}
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
 
-	// Called every time the scheduler runs while the command is scheduled.
-	@Override
-	public void execute() {
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
 
-		// double throt = (((joystick.getRawAxis(3) / 2) + 1) / 2);
+        // double throt = (((joystick.getRawAxis(3) / 2) + 1) / 2);
 
-		// double m_forward = (throt * joystick.getRawAxis(1));
-		// double m_rotation = ((throt / 2) * joystick.getRawAxis(2));
+        // double m_forward = (throt * joystick.getRawAxis(1));
+        // double m_rotation = ((throt / 2) * joystick.getRawAxis(2));
 
-		double throt = (joystick.getRawAxis(3) * -1 + 1);
+        double throt = (joystick.getRawAxis(3) * -1 + 1);
 
-		double m_forward = joystick.getRawAxis(1) * throt * -1;
-		double m_rotation = joystick.getRawAxis(2) * (throt / 2) * -1;
+        double m_forward = joystick.getRawAxis(1) * throt * -1;
+        double m_rotation = joystick.getRawAxis(2) * (throt / 2) * -1;
 
-		System.out.println("throt = " + throt);
-		System.out.println("m_forward = " + m_forward);
-		System.out.println("m_rotation = " + m_rotation);
+        System.out.println("throt = " + throt);
+        System.out.println("m_forward = " + m_forward);
+        System.out.println("m_rotation = " + m_rotation);
 
-		m_drive.arcadeDrive(m_forward, m_rotation);
-	}
+        m_drive.arcadeDrive(m_forward, m_rotation);
+    }
 
-	// Called once the command ends or is interrupted.
-	@Override
-	public void end(boolean interrupted) {
-	}
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
 
-	// Returns true when the command should end.
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
