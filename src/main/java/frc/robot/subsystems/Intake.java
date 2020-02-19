e30dab799ad90163e76b7f9bc351fe5fb31cd287
Lucas Ferguson <LucasFerguson16@gmail.com>
2020-02-18 18:44:02 -0600

    Intake

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+
+  private Solenoid solenoid;
+  private WPI_VictorSPX intakeVictor;
+
+  /**
+   * Creates a new Intake.
+   */
+  public Intake() {
+    solenoid = new Solenoid(0, 0);
+    addChild("solenoid1", solenoid1);
+
+    intakeVictor = new WPI_VictorSPX(Constants.IntakeConstants.INTAKE_VICTOR_CHANNEL);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
