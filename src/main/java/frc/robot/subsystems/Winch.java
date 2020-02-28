1a64ed9f2c1024a127042738a9bac8f129685998
Lucas Ferguson <LucasFerguson16@gmail.com>
2020-02-27 18:04:38 -0600

    Formatting / Removing imports

--- src/main/java/frc/robot/subsystems/Winch.java
+++ src/main/java/frc/robot/subsystems/Winch.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
